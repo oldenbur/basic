1ec6899b8e57d4d8cfdfceda0bc4af9e0908c474
Paul Oldenburg <paul.oldenburg@logrhythm.com>
2018-03-08 22:56:43 -0700

    Attempting to post reservation

--- ycheckin/main.go
+++ ycheckin/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 
 	"time"
@@ -17,7 +20,11 @@ import (
 
 const (
 	regtimesFlag = "regtimes"
-	eventTitle   = "Adult Pick-Up"
+	//eventTitle        = "Adult Pick-Up"
+	eventTitle        = "Adult Pick-Up Hockey (Advanced)"
+	argDateFormat     = "2006-01-02T15:04:05"
+	registrationName  = "Paul Oldenburg"
+	registrationEmail = "oldenbur@gmail.com"
 )
 
 func main() {
@@ -42,7 +49,7 @@ func main() {
 
 func yregister(c *cli.Context) error {
 
-	t, err := time.Parse("2006-01-02T15:04:05", "2018-03-09T07:00:00")
+	t, err := time.Parse(argDateFormat, "2018-03-09T11:00:00")
 	if err != nil {
 		return err
 	}
@@ -52,18 +59,28 @@ func yregister(c *cli.Context) error {
 
 func registerDate(eventTime time.Time) error {
 
-	return nil
+	registerUrl, err := findReserveUrl(eventTime)
+	if err != nil {
+		return err
+	}
+
+	registrationInfo, err := findRegistrationInfo(registerUrl)
+	if err != nil {
+		return err
+	}
+
+	return postRegistration(registrationInfo)
 }
 
-func findRegisterUrl(eventTime time.Time) (string, error) {
+func findReserveUrl(eventTime time.Time) (string, error) {
 
-	var registerUrl string
+	var reserveUrl string
 	var err error
 
 	schedUrl := fmt.Sprintf("https://bouldervalley.consoria.com/%s", eventTime.Format("2006-01-02"))
 	doc, err := goquery.NewDocument(schedUrl)
 	if err != nil {
-		return registerUrl, err
+		return reserveUrl, err
 	}
 
 	startTime := eventTime.Format("3:04 PM")
@@ -81,7 +98,8 @@ func findRegisterUrl(eventTime time.Time) (string, error) {
 				return
 			}
 
-			reserveUrl, exists := s.Find("td > a").Attr("href")
+			var exists bool
+			reserveUrl, exists = s.Find("td > a").Attr("href")
 			if !exists {
 				err = fmt.Errorf("reserveUrl does not exist for %s %s", eventTime, title)
 				log.Print(err.Error())
@@ -91,9 +109,71 @@ func findRegisterUrl(eventTime time.Time) (string, error) {
 			log.Printf("found %s %s %s", eventTime, title, reserveUrl)
 		})
 
-	if registerUrl == "" {
-		err = errors.New("failed to find registerUrl")
+	if reserveUrl == "" {
+		err = errors.New("failed to find reserveUrl")
 	}
 
-	return registerUrl, err
+	return reserveUrl, err
+}
+
+type registrationInfo struct {
+	Action string
+	Token  string
+}
+
+func findRegistrationInfo(registerUrl string) (*registrationInfo, error) {
+
+	var err error
+	var action, token string
+
+	doc, err := goquery.NewDocument(registerUrl)
+	if err != nil {
+		return nil, fmt.Errorf("NewDocument(registerUrl) error: %v", err)
+	}
+
+	doc.Find("form.form").Each(
+		func(i int, s *goquery.Selection) {
+
+			var exists bool
+			action, exists = s.Attr("action")
+			if !exists {
+				err = errors.New("form action not found")
+				log.Print(err.Error())
+				return
+			}
+
+			token, exists = s.Find("input[name=_token]").Attr("value")
+			if !exists {
+				err = errors.New("form token not found")
+				log.Print(err.Error())
+				return
+			}
+
+			log.Printf("action: %s  token: %s", action, token)
+		})
+
+	return &registrationInfo{action, token}, err
+}
+
+func postRegistration(info *registrationInfo) error {
+
+	resp, err := http.PostForm(info.Action,
+		url.Values{
+			"_token": {info.Token},
+			"name":   {registrationName},
+			"email":  {registrationEmail},
+		})
+	if err != nil {
+		return fmt.Errorf("PostForm error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("PostForm resp.Body ReadAll error: %v", err)
+	}
+
+	log.Printf("registration post response: %v", string(body))
+
+	return nil
 }
