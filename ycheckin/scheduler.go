b31655defd311d5f3d1e31e92a5f1dba6fe99cb0
Paul Oldenburg <paul.oldenburg@logrhythm.com>
2018-03-11 21:20:37 -0600

    Reimplemented weekly ticker scheduling

--- ycheckin/scheduler.go
+++ ycheckin/scheduler.go
@@ -11,193 +11,195 @@ import (
 	"github.com/cihub/seelog"
 
 	"io"
-
-	"github.com/jasonlvhit/gocron"
+	"sort"
+	"sync"
 )
 
 const oneMinuteTimeout = 61 * time.Second
 
-var regexpSched = regexp.MustCompile(`(SUN|MON|TUE|WED|THU|FRI|SAT)_(\d{2}\:\d{2}):(\d{2})\.(\d{3})`)
+var regexpSched = regexp.MustCompile(`(SUN|MON|TUE|WED|THU|FRI|SAT)_(\d{2})\:(\d{2}):(\d{2})\.(\d{3})`)
 
 // sched should have the format: TUE_07:00:00.000,FRI_07:00:00.000
 type WeeklyTicker chan time.Time
 
 type WeeklyTickerScheduler interface {
 	io.Closer
-	ParseSchedule(sched string) (WeeklyTicker, error)
+	ScheduleWeekly(sched string) (WeeklyTicker, error)
+	PendingEvents() []time.Time
 }
 
 type weeklyTickerScheduler struct {
-	stopCron  chan bool
-	delayer   func(time.Duration) <-chan time.Time
-	timeouter func() <-chan time.Time
-	firer     func(t WeeklyTicker, eventTime time.Time)
+	mutex         *sync.Mutex
+	pendingEvents timeSlice
+	closeChan     chan bool
 
-	progTimeout func()
+	delayer func(time.Duration) <-chan time.Time
+	firer   func(t WeeklyTicker, eventTime time.Time)
 }
 
 func NewWeeklyTickerScheduler() WeeklyTickerScheduler {
 	return &weeklyTickerScheduler{
+		mutex:         &sync.Mutex{},
+		pendingEvents: timeSlice{},
+		closeChan:     make(chan bool),
 		delayer: func(d time.Duration) <-chan time.Time {
 			return time.After(d)
-		},
-		timeouter: func() <-chan time.Time {
-			return time.NewTicker(oneMinuteTimeout).C
 		},
 		firer: func(t WeeklyTicker, eventTime time.Time) {
 			t <- eventTime
 		},
-		progTimeout: func() {},
 	}
 }
 
-func (s *weeklyTickerScheduler) ParseSchedule(sched string) (WeeklyTicker, error) {
+func (s *weeklyTickerScheduler) ScheduleWeekly(sched string) (WeeklyTicker, error) {
 
-	var err error
 	ticker := make(WeeklyTicker)
+	var err error
 
-	for i, pat := range strings.Split(sched, ",") {
+	s.pendingEvents, _ = s.buildPendingEventQueue(sched)
 
-		seelog.Infof(`adding ticker for pattern %v`, pat)
-
-		regexpMat := regexpSched.FindStringSubmatch(pat)
-		if len(regexpMat) != 5 {
-			err = fmt.Errorf(`failed to parse schedule component %d "%s"  matches: %v`, i, pat, regexpMat)
-		}
-
-		var job *gocron.Job
-		job, err = s.createJobByDay(regexpMat[1])
-		if err != nil {
-			seelog.Warnf(`createJobByDay for %s error: %v`, pat, err)
-			continue
-		}
-		seelog.Debugf(`created job on day %s`, regexpMat[1])
-
-		job, err = s.setJobTime(job, regexpMat[2])
-		if err != nil {
-			seelog.Warnf(`setJobTime(%s) for %s error: %v`, regexpMat[2], pat, err)
-			continue
-		}
-		seelog.Debugf(`set job time to %s`, regexpMat[2])
-
-		var delay time.Duration
-		delay, err = s.buildSubMinuteDelay(regexpMat[3], regexpMat[4])
-		if err != nil {
-			seelog.Warnf(`buildSubMinuteDelay(%s, %s) for %s error: %v`, regexpMat[3], regexpMat[4], pat, err)
-			continue
-		}
-		seelog.Debugf(`added job delay %v`, delay)
-
-		job.Do(s.fireWeeklyTicker, ticker, delay)
+	if len(s.pendingEvents) < 1 {
+		return ticker, fmt.Errorf("schedule contains no events")
 	}
 
-	// TODO: do not ignore the return value, do cleanup properly
-	s.stopCron = gocron.Start()
+	go func() {
+
+		for {
+
+			event := s.rollEvent()
+			delay := event.Sub(time.Now())
+			eventChan := s.delayer(delay)
+			seelog.Infof("waiting %v for next event: %v", delay, event)
+
+			select {
+			case eventTime := <-eventChan:
+				seelog.Infof("firing event at %v", eventTime)
+				s.firer(ticker, eventTime)
+				break
+
+			case <-s.closeChan:
+				seelog.Infof("closing event loop")
+				return
+			}
+		}
+
+	}()
 
 	return ticker, err
 }
 
-func (s *weeklyTickerScheduler) Close() error {
-	timeoutC := s.timeouter()
+func (s *weeklyTickerScheduler) PendingEvents() []time.Time {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
-	select {
-	case s.stopCron <- true:
-		seelog.Infof("stopped gocron")
-	case <-timeoutC:
-		return seelog.Warn("timed out stopping gocron")
+	events := make(timeSlice, len(s.pendingEvents))
+	copy(events, s.pendingEvents)
+
+	return events
+}
+
+func (s *weeklyTickerScheduler) rollEvent() time.Time {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	t := s.pendingEvents[0]
+	s.pendingEvents = append(
+		s.pendingEvents[1:],
+		time.Date(t.Year(), t.Month(), t.Day()+7, t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local),
+	)
+
+	return t
+}
+
+func (s *weeklyTickerScheduler) buildPendingEventQueue(sched string) (timeSlice, error) {
+
+	var err error
+	events := timeSlice{}
+
+	for _, pat := range strings.Split(sched, ",") {
+
+		seelog.Infof(`adding ticker for pattern %v`, pat)
+
+		regexpMat := regexpSched.FindStringSubmatch(pat)
+		if len(regexpMat) != 6 {
+			err = seelog.Warnf(`unexpected structure parsing schedule component '%s' matches: %v`, pat, regexpMat)
+		}
+
+		var event time.Time
+		if event, err = s.buildPendingDate(regexpMat[1], regexpMat[2], regexpMat[3], regexpMat[4], regexpMat[5]); err != nil {
+			err = seelog.Warnf(`buildPendingDate error: %v`, err)
+		}
+
+		events = append(events, event)
 	}
 
+	sort.Sort(events)
+
+	return events, err
+}
+
+func (s *weeklyTickerScheduler) Close() error {
 	return nil
 }
 
-func (s *weeklyTickerScheduler) createJobByDay(dayCode string) (*gocron.Job, error) {
-
-	job := gocron.Every(1)
-
-	switch dayCode {
+func (s *weeklyTickerScheduler) dayStringToWeekday(day string) (time.Weekday, error) {
+	switch day {
 	case "SUN":
-		return job.Sunday(), nil
+		return time.Sunday, nil
 	case "MON":
-		return job.Monday(), nil
+		return time.Monday, nil
 	case "TUE":
-		return job.Tuesday(), nil
+		return time.Tuesday, nil
 	case "WED":
-		return job.Wednesday(), nil
+		return time.Wednesday, nil
 	case "THU":
-		return job.Thursday(), nil
+		return time.Thursday, nil
 	case "FRI":
-		return job.Friday(), nil
+		return time.Friday, nil
 	case "SAT":
-		return job.Saturday(), nil
+		return time.Saturday, nil
+	default:
+		return time.Sunday, fmt.Errorf("invalid day string: %s", day)
 	}
-
-	return nil, fmt.Errorf("unrecognized dayCode: %s", dayCode)
 }
 
-func (s *weeklyTickerScheduler) setJobTime(job *gocron.Job, tod string) (*gocron.Job, error) {
+func (s *weeklyTickerScheduler) buildPendingDate(dayStr, hourStr, minStr, secStr, msStr string) (time.Time, error) {
 
 	var err error
-	var ok bool
+	var t time.Time
+	var targetDay time.Weekday
 
-	func() {
-		defer func() {
-			if r := recover(); r != nil {
-				if err, ok = r.(error); !ok {
-					seelog.Warnf(`setJobTime(%s) unexpected recover artifact: %v`, tod, r)
-				} else {
-					err = fmt.Errorf(`setJobTime(%s) error: %v`, tod, err)
-				}
-			}
-		}()
-
-		job.At(tod)
-	}()
-
-	return job, err
-}
-
-func (s *weeklyTickerScheduler) buildSubMinuteDelay(secsStr, millisStr string) (time.Duration, error) {
-
-	var dur time.Duration
-	var secs, millis int
-	var err error
-
-	if secs, err = strconv.Atoi(secsStr); err != nil {
-		return dur, fmt.Errorf("Atoi(%s) error: %v", secsStr, err)
+	if targetDay, err = s.dayStringToWeekday(dayStr); err != nil {
+		return t, fmt.Errorf(`dayString '%s' error: %v`, dayStr, err)
 	}
 
-	if millis, err = strconv.Atoi(millisStr); err != nil {
-		return dur, fmt.Errorf("Atoi(%s) error: %v", millisStr, err)
+	var hour, min, sec, ms int
+	if hour, err = strconv.Atoi(hourStr); err != nil {
+		return t, fmt.Errorf(`hour '%s' error: %v`, hourStr, err)
+	}
+	if min, err = strconv.Atoi(minStr); err != nil {
+		return t, fmt.Errorf(`min '%s' error: %v`, minStr, err)
+	}
+	if sec, err = strconv.Atoi(secStr); err != nil {
+		return t, fmt.Errorf(`sec '%s' error: %v`, secStr, err)
+	}
+	if ms, err = strconv.Atoi(msStr); err != nil {
+		return t, fmt.Errorf(`ms '%s' error: %v`, msStr, err)
 	}
 
-	dur = time.Duration(secs)*time.Second + time.Duration(millis)*time.Millisecond
-
-	return dur, nil
-}
-
-func (s *weeklyTickerScheduler) fireWeeklyTicker(ticker WeeklyTicker, delay time.Duration) {
-
-	delayC := s.delayer(delay)
-	timeoutC := s.timeouter()
-
-	seelog.Infof(`firing ticker in %v...`, delay)
-
-	if delay <= time.Duration(0) {
-
-		now := time.Now()
-		seelog.Infof(`firing ticker at %v`, now)
-		s.firer(ticker, now)
-
-	} else {
-
-		select {
-		case now := <-delayC:
-			seelog.Infof(`firing ticker at %v`, now)
-			s.firer(ticker, now)
-
-		case now := <-timeoutC:
-			seelog.Warnf(`fireWeeklyTicker(%v) timed out at: %v`, delay, now)
-			s.progTimeout()
-		}
+	now := time.Now()
+	t = time.Date(now.Year(), now.Month(), now.Day(), hour, min, sec, ms*1000000, time.Local)
+	for t.Weekday() != targetDay {
+		seelog.Tracef(`seeking %v: %v`, targetDay, t)
+		t = time.Date(t.Year(), t.Month(), t.Day()+1, t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local)
 	}
+	seelog.Debugf(`pending date %v: %v`, targetDay, t)
+
+	return t, nil
 }
+
+type timeSlice []time.Time
+
+func (s timeSlice) Len() int           { return len(s) }
+func (s timeSlice) Less(i, j int) bool { return s[i].Before(s[j]) }
+func (s timeSlice) Swap(i, j int)      { t := s[i]; s[i] = s[j]; s[j] = t }
--- ycheckin/scheduler_test.go
+++ ycheckin/scheduler_test.go
@@ -6,45 +6,31 @@ import (
 	"time"
 
 	"github.com/cihub/seelog"
-	"github.com/jasonlvhit/gocron"
 	"github.com/stretchr/testify/assert"
+	"math"
 )
 
 func init() { setupLogging() }
 
 func TestParseSchedule(t *testing.T) {
+	defer seelog.Flush()
 	s := NewWeeklyTickerScheduler()
 
-	future := time.Now().Add(time.Minute)
-	pat := fmt.Sprintf(`%s_%s:00.000`, weekdayToPat(future.Weekday()), future.Format("15:04"))
+	future := time.Now().Add(500 * time.Millisecond)
+	pat := fmt.Sprintf(`%s_%s`, weekdayToPat(future.Weekday()), future.Format("15:04:05.000"))
 
-	ticker, err := s.ParseSchedule(pat)
+	ticker, err := s.ScheduleWeekly(pat)
 	assert.Nil(t, err)
 	assert.NotNil(t, ticker)
 
-	tick := <-ticker
-	assert.Equal(t, tick, future)
-}
+	for i, e := range s.PendingEvents() {
+		seelog.Infof("pendingEvent[%d]: %v", i, e)
+	}
 
-func TestGocron(t *testing.T) {
-	c := make(chan int)
-	seelog.Infof("now: %v", time.Now())
-	gocron.Every(1).Sunday().At("10:19").Do(func() {
-		seelog.Infof("gocron1 works: %v", time.Now())
-		c <- 1
-	})
-	gocron.Every(1).Sunday().At("11:19").Do(func() {
-		seelog.Infof("gocron2 works: %v", time.Now())
-		c <- 2
-	})
-	gocron.Every(1).Sunday().At("12:19").Do(func() {
-		seelog.Infof("gocron3 works: %v", time.Now())
-		c <- 3
-	})
-	gocron.Start()
-	job, nextTime := gocron.NextRun()
-	seelog.Infof("nextTime: %v  job: %v", nextTime, job)
-	<-c
+	tick := <-ticker
+	delta := tick.Sub(future)
+	seelog.Infof("delta: %v", delta)
+	assert.True(t, math.Abs(float64(delta)) < float64(10*time.Millisecond))
 }
 
 func weekdayToPat(w time.Weekday) string {
