2dd872fe76397f94da1c9a98da660e3363361365
Paul Oldenburg <oldenbur@gmail.com>
2018-10-06 07:32:43 -0600

    Write logging to img_copy.log

--- img_copy/img_copy.py
+++ img_copy/img_copy.py
@@ -17,6 +17,7 @@ parser.add_argument('-d', '--dryrun', action="store_true",
                     help='Don\'t upload anything')
 
 logging.basicConfig(
+    filename='img_copy.log',
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s', 
     datefmt='%Y-%m-%d %H:%M:%S'
