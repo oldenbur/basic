f3cb021965c6054d2fa19c7a4912d6009f1abc0b
Paul Oldenburg <bucky@localhost.localdomain>
2018-11-28 06:02:36 -0700

    Added jscratch stub gradle project

---
+++ jscratch/src/main/java/net/pgoldenb/JScratch.java
@@ -0,0 +1,8 @@
+package net.pgoldenb;
+
+public class JScratch {
+    public static void main(String[] args) {
+        System.out.println("JScratch Starting");
+        System.out.println("JScratch Complete");
+    }
+}
