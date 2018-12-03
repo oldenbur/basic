e66a988314f4f154cae0e288198a668ffbdd232f
Paul Oldenburg <bucky@localhost.localdomain>
2018-12-02 23:03:46 -0700

    Refactored practice centipede

--- jscratch/src/test/java/net/pgoldenb/CentipedeTest.java
+++ jscratch/src/test/java/net/pgoldenb/CentipedeTest.java
@@ -1,10 +1,10 @@
 package net.pgoldenb;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class CentipedeTest {
 
@@ -29,73 +29,193 @@ public class CentipedeTest {
         System.out.println(c.toString());
     }
 
+    @Test
+    public void boardLocRequiresEquals() {
+        Set<boardLoc> m = new HashSet<>();
+        m.add(new boardLoc(1, 2));
+        assertTrue(m.contains(new boardLoc(1, 2)));
+    }
+
 }
 
+/**
+ * Requirements:
+ *   * does board cell contain segment?
+ *   * where is the head and tail?
+ *   * move: new head segment in unoccupied direction, tail segment disappears and becomes previous second-to-last
+ *   * grow: new head segment in unoccupied direction
+ */
+
+
+class boardLoc {
+    int x, y;
+
+    public boardLoc(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isAdjacent(boardLoc otherLoc) {
+        int xDelta = Math.abs(x - otherLoc.x);
+        int yDelta = Math.abs(y - otherLoc.y);
+        return (xDelta == 1 && yDelta == 0) || (xDelta == 0 && yDelta == 1);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    @Override public boolean equals(Object o) {
+        return (o != null && o instanceof boardLoc && ((boardLoc)o).x == x && ((boardLoc)o).y == y);
+    }
+
+    @Override public int hashCode() {
+        int result = 17;
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
+    }
+}
+
+class creature {
+
+    private List<boardLoc> segList;
+    private Set<boardLoc> segSet;
+
+    public creature(List<boardLoc> locs, int boardDim) {
+        segList = new LinkedList<>();
+        segSet = new HashSet<>(locs.size());
+
+        for (boardLoc loc : locs) {
+            validateCreatureSegmentAdjacency(loc);
+            validateCreatureSegmentConflict(loc);
+            validateCreatureSegmentOnBoard(loc, boardDim);
+
+            segList.add(loc);
+            segSet.add(loc);
+        }
+    }
+
+    private void validateCreatureSegmentOnBoard(boardLoc loc, int boardDim) {
+        if (loc.x < 0 || loc.x >= boardDim || loc.y < 0 || loc.y >= boardDim) {
+            throw new IllegalArgumentException(String.format("segment %s not on board of dimension %d", loc, boardDim));
+        }
+    }
+
+    private void validateCreatureSegmentConflict(boardLoc loc) {
+        if (segSet.contains(loc)) {
+            throw new IllegalArgumentException(String.format("initial segment conflict: %s", loc));
+        }
+    }
+
+    private void validateCreatureSegmentAdjacency(boardLoc loc) {
+
+        boardLoc prevLoc = null;
+        if (segList.size() > 0) {
+            prevLoc = segList.get(segList.size()-1);
+        }
+
+        if (prevLoc != null && !loc.isAdjacent(prevLoc)) {
+            throw new IllegalArgumentException(
+                    String.format("initial location segments not adjacent: %s vs %s", prevLoc, loc)
+            );
+        }
+    }
+
+    public boolean containsLocation(boardLoc loc) {
+        return segSet.contains(loc);
+    }
+
+    public int length() {
+        return segList.size();
+    }
+
+    // TODO
+    public creature moveTo(boardLoc loc) {
+        return null;
+    }
+}
 
 class Centipede {
 
 //    enum CentipedeBoardVal { EMPTY, CREATURE }
+    private int boardDim;
+    private creature curCreature;
 
-    class boardLoc implements Comparable<boardLoc> {
-        int i, j;
+    public Centipede(int boardDim, int centLen, List<boardLoc> initCreature) {
+        validateBoardDimAndCentLen(boardDim, centLen);
 
-        public boardLoc(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-
-        @Override
-        public int compareTo(boardLoc o) {
-            if (i != o.i) return i - o.i;
-            else return (j - o.j);
-        }
+        this.boardDim = boardDim;
+        this.curCreature = new creature(initCreature, boardDim);
     }
 
-    private SortedMap<boardLoc, Integer> creature;
-    private int boardDim;
-
     public Centipede(int boardDim, int centLen) {
-        this.boardDim = boardDim;
+        this(boardDim, centLen, defaultInitCreature(boardDim, centLen));
+    }
 
+    private static void validateBoardDimAndCentLen(int boardDim, int centLen) {
         if (boardDim < 3) {
             throw new IllegalArgumentException("boardDim must be greater than 3");
         }
-        if (centLen < 1 || centLen > (boardDim/2)) {
-            throw new IllegalArgumentException(String.format("centLen must be 1 <= centLen <= %d", (boardDim/2)));
-        }
-
-        int ordI = (boardDim/2);
-        int ordJ = (boardDim/2);
-        creature = new TreeMap<>();
-        for (int l=0; l < centLen; l++) {
-            creature.put(new boardLoc(ordI, ordJ - l), Integer.valueOf(l));
+        if (centLen < 1 || centLen > (boardDim / 2)) {
+            throw new IllegalArgumentException(String.format("centLen must be 1 <= centLen <= %d", (boardDim / 2)));
         }
     }
 
-    public int length() {
-        return creature.size();
+    private static List<boardLoc> defaultInitCreature(int boardDim, int centLen) {
+
+        int ordX = (boardDim/2);
+        int ordY = (boardDim/2);
+
+        List<boardLoc> cLoc = new ArrayList<>(centLen);
+        for (int l=0; l < centLen; l++) {
+            cLoc.add(new boardLoc(ordX - l, ordY));
+        }
+
+        return cLoc;
     }
 
     public String toString() {
         StringBuffer b = new StringBuffer();
         b.append("+" + repeat("-", boardDim) + "+\n");
-        for (int j=0; j < boardDim; j++) {
-            for (int i=0; i < boardDim; i++) {
-                boardLoc loc = new boardLoc(j, i);
-                if (!creature.containsKey(loc)) {
-                    b.append(" ");
-                } else {
-                    b.append("*");
-                }
-            }
-            b.append("\n");
-        }
+        visitBoardCells(
+                boardDim,
+                i -> b.append("|"),
+                l -> {
+                    if (curCreature.containsLocation(l)) {
+                        String xStr = String.format("%d", l.x);
+                        b.append(xStr.substring(xStr.length()-1));
+                    } else {
+                        b.append(" ");
+                    }
+                },
+                i -> b.append("|\n")
+        );
         b.append("+" + repeat("-", boardDim) + "+\n");
 
         return b.toString();
     }
 
-    public String repeat(String s, int n) {
+    private static void visitBoardCells(
+            int boardDim,
+            Consumer<Integer> rowBeginConsumer,
+            Consumer<boardLoc> cellConsumer,
+            Consumer<Integer> rowEndConsumer
+    ) {
+        for (int y=0; y < boardDim; y++) {
+            rowBeginConsumer.accept(y);
+            for (int x=0; x < boardDim; x++) {
+                cellConsumer.accept(new boardLoc(x, y));
+            }
+            rowEndConsumer.accept(y);
+        }
+    }
+
+    public int length() {
+        return curCreature.length();
+    }
+
+    public static String repeat(String s, int n) {
         return new String(new char[n]).replace("\0", s);
     }
 }
