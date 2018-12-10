1f6adcc0063bdb1a4dccb81b95ac3997ad7cae03
Paul Oldenburg <oldenbur@gmail.com>
2018-12-10 06:01:12 -0700

    LinkedListTree practice visitor

--- jscratch/src/test/java/net/pgoldenb/LinkedListTreeTest.java
+++ jscratch/src/test/java/net/pgoldenb/LinkedListTreeTest.java
@@ -6,14 +6,16 @@ import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinkedListTreeTest {
 
     @Test
     public void testLinkedListTree() throws FileNotFoundException {
-        Gson gson = new Gson();
-        LLTNode head = gson.fromJson(new FileReader("llt_test1.json"), LLTNode.class);
-        System.out.format("head: %s%n", head);
+        LLTNode tree = LLTNode.buildLLTree(new FileReader("llt_test1.json"));
+        System.out.format("head: %s%n", tree);
     }
 
 
@@ -24,17 +26,30 @@ class LLTNode {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     private String data;
-    private String tag;
-    private String prevTag;
+    private LLTCoord coord;
 
     private transient LLTNode prev;
     private LLTNode next;
     private LLTNode child;
 
-    public LLTNode(String data, LLTNode next, LLTNode child) {
+    public static LLTNode buildLLTree(Reader r) {
+        LLTNode root = gson.fromJson(r, LLTNode.class);
+
+        root.coord = LLTCoord.rootValue();
+        visitLLTree(root, (parent, prev, cur) -> {
+            if (parent != null) {
+                cur.coord = LLTCoord.childOf(parent.coord);
+            } else if (prev != null) {
+                cur.coord = LLTCoord.nextOf(prev.coord);
+            }
+            cur.prev = prev;
+        });
+
+        return root;
+    }
+
+    private LLTNode(String data, LLTNode next, LLTNode child) {
         this.data = data;
-        this.prevTag = prevTag;
-        this.prev = prev;
         this.next = next;
         this.child = child;
     }
@@ -43,4 +58,69 @@ class LLTNode {
         return gson.toJson(this);
     }
 
+    interface LLTVisitor {
+        void visit(LLTNode parent, LLTNode prev, LLTNode cur);
+    }
+
+    private static void visitLLTree(LLTNode root, LLTVisitor v) {
+
+        if (root.next != null) {
+            v.visit(null, root, root.next);
+            visitLLTree(root.next, v);
+        }
+        if (root.child != null) {
+            v.visit(root, null, root.child);
+            visitLLTree(root.child, v);
+        }
+    }
+
+    private static void printLLTPrevs(LLTNode n) {
+        List<String> prevData = new ArrayList<>();
+        LLTNode c = n;
+
+        prevData.add(c.data);
+        while (c.prev != null) {
+            c = c.prev;
+            prevData.add(c.data);
+        }
+
+        System.out.println(prevData);
+    }
+
+    static class LLTCoord {
+        private List<Integer> coord;
+
+        private LLTCoord(List<Integer> coord) {
+            this.coord = coord;
+        }
+
+        public static LLTCoord rootValue() {
+            LLTCoord c = new LLTCoord(new ArrayList<>());
+            c.coord.add(1);
+            return c;
+        }
+
+        public static LLTCoord childOf(LLTCoord c) {
+            LLTCoord child = new LLTCoord(c.coord);
+            child.coord.add(1);
+            return child;
+        }
+
+        public static LLTCoord nextOf(LLTCoord c) {
+            LLTCoord next = new LLTCoord(c.coord);
+            int lastIndex = next.coord.size() - 1;
+            next.coord.set(lastIndex, next.coord.get(lastIndex) + 1);
+            return next;
+        }
+
+        public String toString() {
+            String s = coord.get(0).toString();
+            if (coord.size() > 1) {
+                for (Integer i : coord.subList(1, coord.size())) {
+                    s = s + "." + i.toString();
+                }
+            }
+            return s;
+        }
+    }
 }
