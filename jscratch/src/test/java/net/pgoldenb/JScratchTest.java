ec7edd764ad8d5e741bdda49c0b44cef12ebfca3
Paul Oldenburg <oldenbur@gmail.com>
2018-12-28 15:21:41 -0700

    Practice tree implementation

--- jscratch/src/test/java/net/pgoldenb/JScratchTest.java
+++ jscratch/src/test/java/net/pgoldenb/JScratchTest.java
@@ -1,8 +1,5 @@
 package net.pgoldenb;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
@@ -11,17 +8,37 @@ import static org.junit.Assert.assertEquals;
 
 public class JScratchTest {
 
-    public static final int DIVISOR = 1000000;
-
     @Test
-    public void test1() throws InterruptedException {
-        int[] input1 = {1, 2, 3, 4, 5}; assertEquals(2, Arrays.binarySearch(input1, 3));
-        int[] input2 = {1, 2, 2, 3, 4, 4, 5}; int i = Arrays.binarySearch(input2, 2);
-            Assert.assertTrue(1 <= i); Assert.assertTrue(i <= 2);
-        int[] input3 = {1, 2, 4, 5}; assertEquals(-3, Arrays.binarySearch(input3, 3));
-        int[] input4 = {1, 2, 3, 4, 5}; assertEquals(-1, Arrays.binarySearch(input4, 0));
-        int[] input5 = {1, 2, 3, 4, 5}; assertEquals(-6, Arrays.binarySearch(input5, 7));
-//        int[] input6 = {1, 2, 3, 4, 5}; assertEquals(2, Arrays.binarySearch(input6, 3));
+    public void test1() {
+        assertEquals("ttr", removeChars("teeter", "aeiou"));
+        assertEquals("abcegecba", removeChars("abcdefgfedcba", "df"));
+        assertEquals("Bttl f th Vwls: Hw vs. Grzny",
+                removeChars("Battle of the Vowels: Hawaii vs. Grozny", "aeiou"));
     }
 
+    @Test(expected = ArithmeticException.class)
+    public void testThrows() throws InterruptedException {
+        int i = 10 / 0;
+        System.out.printf("%d%n", i);
+    }
+
+    private String removeChars(String str, String remove) {
+
+        Set<Integer> removeSet = new HashSet<>();
+        for (int i=0; i < remove.length();) {
+            int cp = remove.codePointAt(i);
+            i += Character.charCount(cp);
+            removeSet.add(cp);
+        }
+
+        StringBuilder buf = new StringBuilder();
+        for (int i=0; i < str.length();) {
+            int cp = str.codePointAt(i);
+            i += Character.charCount(cp);
+            if (!removeSet.contains(cp)) {
+                buf.append(Character.toChars(cp));
+            }
+        }
+        return buf.toString();
+    }
 }
--- jscratch/src/test/java/net/pgoldenb/TreeTest.java
+++ jscratch/src/test/java/net/pgoldenb/TreeTest.java
@@ -7,18 +7,38 @@ import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class TreeTest {
 
     @Test
-    public void test1() throws FileNotFoundException {
+    public void testMaxHeight() throws FileNotFoundException {
         TreeNode<String> tree = TreeNode.buildTree(new FileReader("tree_test1.json"));
-        System.out.format("head: %s%n", tree);
-        assertEquals(6, TreeNode.maxHeight(tree, 0, 0));
+//        System.out.format("head: %s%n", tree);
+        assertEquals(6, TreeNode.maxHeight(tree));
+    }
+
+    @Test
+    public void testPreorder() throws FileNotFoundException {
+        TreeNode<String> tree = TreeNode.buildTree(new FileReader("tree_test2.json"));
+        System.out.println("testPreorder:");
+        TreeNode.preorder(tree);
+    }
+
+    @Test
+    public void testPreorderIter() throws FileNotFoundException {
+        TreeNode<String> tree = TreeNode.buildTree(new FileReader("tree_test2.json"));
+        System.out.println("testPreorderIter:");
+        TreeNode.preorderIter(tree);
+    }
+
+    @Test
+    public void findAncestor() throws FileNotFoundException {
+        TreeNode<String> tree = TreeNode.buildTree(new FileReader("tree_test3.json"));
+        TreeNode<String> a = TreeNode.findNode(tree, "8");
+        assertEquals(a, TreeNode.commonAncestor(tree, "14", "4"));
     }
 
 }
@@ -35,20 +55,87 @@ class TreeNode<T> {
         return root;
     }
 
-    public static <T> int maxHeight(TreeNode<T> root, int curHeight, int maxHeight) {
+    public static <T> int maxHeight(TreeNode<T> root) {
 
-        curHeight += 1;
-        int newMax = curHeight;
+        int newMax = 1;
         for (TreeNode<T> child : root.children) {
-            int childHeight = maxHeight(child, curHeight, newMax);
+            int childHeight = maxHeight(child) + 1;
             if (childHeight > newMax) newMax = childHeight;
          }
 
         return newMax;
     }
 
+    public static <T> void preorder(TreeNode<T> root) {
+        System.out.println(root.data);
+        for (TreeNode<T> child : root.children) {
+            preorder(child);
+        }
+    }
+
+    public static <T> void preorderIter(TreeNode<T> root) {
+        Deque<TreeNode<T>> deque = new LinkedList<TreeNode<T>>();
+        deque.addFirst(root);
+        while (!deque.isEmpty()) {
+            TreeNode<T> cur = deque.removeFirst();
+            System.out.println(cur.data);
+            for (int i = cur.children.size() - 1; i >= 0; i--)
+                deque.addFirst(cur.children.get(i));
+        }
+    }
+
+    public static <T> TreeNode<T> findNode(TreeNode<T> root, T val) {
+
+        if (root.data.equals(val)) return root;
+        for (TreeNode<T> child : root.children) {
+            TreeNode<T> n = findNode(child, val);
+            if (n != null) return n;
+        }
+        return null;
+    }
+
+    public static <T> Map<TreeNode<T>,TreeNode<T>> parentMap(
+            Map<TreeNode<T>,TreeNode<T>> parents,
+            TreeNode<T> parent,
+            TreeNode<T> cur
+    ) {
+
+        if (parents == null) parents = new HashMap<>();
+        if (parent != null) parents.put(cur, parent);
+        for (TreeNode<T> child : cur.children) parents = parentMap(parents, cur, child);
+
+        return parents;
+    }
+
+    public static <T> TreeNode<T> commonAncestor(TreeNode<T> root, T val1, T val2) {
+
+        TreeNode<T> n1 = findNode(root, val1);
+        if (n1 == null) return null;
+
+        TreeNode<T> n2 = findNode(root, val2);
+        if (n2 == null) return null;
+
+        Map<TreeNode<T>,TreeNode<T>> parents = parentMap(null, null, root);
+
+        Set<TreeNode<T>> n1Parents = new HashSet<>();
+        TreeNode<T> n1Parent = parents.get(n1);
+        while (n1Parent != null) {
+            n1Parents.add(n1Parent);
+            n1Parent = parents.get(n1Parent);
+        }
+
+        TreeNode<T> n2Parent = parents.get(n2);
+        while(n2Parent != null) {
+            if (n1Parents.contains(n2Parent)) return n2Parent;
+            n2Parent = parents.get(n2Parent);
+        }
+
+        return null;
+    }
+
     public String toString() {
-        return gson.toJson(this);
+//        return gson.toJson(this);
+        return data.toString();
     }
 
 }
