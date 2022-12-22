b2647ba9bb516a1f7231ad5ac11cad2e293963d7
Paul Oldenburg <oldenbur@gmail.com>
2022-12-22 08:25:09 -0700

    Advent 2022 days 3-8

--- jsscratch/advent/2022d3.js
+++ jsscratch/advent/2022d3.js
@@ -3,35 +3,75 @@
 
 var fs = require('fs');
 
-const processLine = line => {
-  l1 = line.substring(0, line.length/2)
-  l2 = line.substring(line.length/2)
-
+const commonItems = (l1, l2) => {
   const s = new Set()
   Array.from(l1).forEach(c => s.add(c))
-  item = Array.from(l2).filter(c => s.has(c))[0]
-
-  let p = item.charCodeAt()
-  let pp = p < 97 ? p - 38 : p - 96;
-  //console.log(`l1: ${l1}  l2: ${l2}  item: ${item}  pp: ${pp}`)
-  return pp
+  return Array.from(l2).filter(c => s.has(c))
 }
 
-// A: 65 (-38) a: 97 (-96)
+const commonItemsAll = (lists) => {
+  return lists.reduce(
+    (common, list) => {
+      const result = list.length < 1
+	? common
+	: common.length < 1
+	  ? list
+	  : commonItems(common, list);
+      //console.log(`common: ${common}  list: ${list}  result: ${result}`)
+      return result
+    },
+    []);
+}
+
+const scoreItem = item => {
+  // A: 65 (-38) a: 97 (-96)
+  let p = item.charCodeAt()
+  return p < 97 ? p - 38 : p - 96;
+}
+
+const processLinePart1 = line => {
+  const l1 = line.substring(0, line.length/2)
+  const l2 = line.substring(line.length/2)
+  const item = commonItemsAll([l1, l2])[0]
+  //console.log(`item: ${item}  type: ${JSON.stringify(item)}`)
+
+  return scoreItem(item) 
+}
 
 fs.readFile('2022d3.data', 'utf8', (err, data) => {
   if (err) throw err;
 
-  let pp = 0;
   const lines = data.split('\n');
-  for (let line of lines) {
-    if (line === '') {
-      continue
-    }
+  const pp = lines.reduce(
+    (sum, line) => (line === '') ? sum : sum + processLinePart1(line),
+    0)
+  console.log(`part1: ${pp}`)
+});
 
-    pp += processLine(line)
+fs.readFile('2022d3_2.data', 'utf8', (err, data) => {
+  if (err) throw err;
 
-  }
-  console.log(`priority: ${pp}`)
+  const lines = data.split('\n');
+  const groups = lines.filter(l => l !== '').reduce(
+    (g, line) => {
+      //console.log(`before - g: ${JSON.stringify(g)}  line: ${line}`)
+      if (g.length == 0 || g[g.length-1].length % 3 == 0) {
+        g.push([line])
+      } else {
+        g[g.length - 1].push(line)
+      }
+      //console.log(`after - g: ${JSON.stringify(g)}`)
+      return g
+    },
+    [])
+  const pp = groups.reduce(
+    (sum, group) => {
+      const item = commonItemsAll(group)[0]
+      const score = scoreItem(item)
+      //console.log(`group: ${group}  score: ${score}  sum: ${sum}`)
+      return sum + score
+    },
+    0)
+  console.log(`part2: ${pp}`)
 });
 
