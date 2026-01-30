3f4c900cd4651e770473268ba8c8256cd38b0060
Paul Oldenburg <oldenbur@gmail.com>
2026-01-30 19:25:17 +0000

    Tinkering with crawler libraries.

--- python/ycheckin/server.py
+++ python/ycheckin/server.py
@@ -1,4 +1,7 @@
 import flask
+import pathlib
+import scrapy
+
 
 app = flask.Flask(__name__)
 
@@ -6,3 +9,12 @@ app = flask.Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+class SignupGeniusSpider(scrapy.Spider):
+
+    ymca_signup_url = "https://www.signupgenius.com/go/10C0B4BA4AC29AAF9CF8-49049901-adult#/"
+
+    async def start(self):
+        return scrapy.Request(url="https://www.signupgenius.com/", callback=self.parse)
+    
+    def parse(self, response):
